a0184537b61c63aef73906a4b1978db2aede63e3
Nhlakanipho Ndlanzi <160495606+ndlanzi-c137@users.noreply.github.com>
2024-10-04 19:41:31 +0200

    cmment

--- src/js/mealCredit.js
+++ src/js/mealCredit.js
@@ -24,31 +24,31 @@ function fetchCredits() {
         });
 }
 
-// function fetchCredits() {
-//     fetch('/api/credits/remaining')
-//     .then((response) => {
-//         if (response.status === 401) {
-//           // Redirect to login page if user is not authorized
-//           window.location.href = "/login";
-//           throw new Error("Unauthorized access. Redirecting to login...");
-//         }
-//         return response.json();
-//       })
-//         .then(data => {
-//             const creditsElement = document.getElementById('credits-amount');
+function fetchCredits() {
+    fetch('/api/credits/remaining')
+    .then((response) => {
+        if (response.status === 401) {
+          // Redirect to login page if user is not authorized
+          window.location.href = "/login";
+          throw new Error("Unauthorized access. Redirecting to login...");
+        }
+        return response.json();
+      })
+        .then(data => {
+            const creditsElement = document.getElementById('credits-amount');
 
-//             // Check if remaining_credits is present in the response data
-//             if (data && typeof data.remaining_credits !== 'undefined') {
-//                 creditsElement.innerText = `R ${parseFloat(data.remaining_credits).toFixed(2)}`;
-//             } else {
-//                 creditsElement.innerText = 'No credits available';
-//             }
-//         })
-//         .catch(error => {
-//             console.error('Error fetching remaining credits:', error);
-//             document.getElementById('credits-amount').innerText = 'Error fetching credits';
-//         });
-// }
+            // Check if remaining_credits is present in the response data
+            if (data && typeof data.remaining_credits !== 'undefined') {
+                creditsElement.innerText = `R ${parseFloat(data.remaining_credits).toFixed(2)}`;
+            } else {
+                creditsElement.innerText = 'No credits available';
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching remaining credits:', error);
+            document.getElementById('credits-amount').innerText = 'Error fetching credits';
+        });
+}
 
 // Fetch and display recent transactions
 function fetchRecentTransactions() {
